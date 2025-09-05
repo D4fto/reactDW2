45c172b3c22c177cdbbdb98cf2c119981ebc18d1
D4fto <163424876+D4fto@users.noreply.github.com>
2025-09-05 12:01:44 -0300

    jup

---
+++ Contador/src/App.jsx
@@ -0,0 +1,13 @@
+import "./styles/global.css";
+import "./styles/theme.css";
+import Contador from "./components/Contador";
+
+function App() {
+  return (
+    <>
+      <Contador/>
+    </>
+  );
+}
+
+export default App;
